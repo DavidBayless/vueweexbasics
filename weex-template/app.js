684ebd6ae2be12aa0634184566023aea080e957e
Brian Cullinan <megamindbrian@gmail.com>
2017-03-24 22:16:11 -0700

    no message

--- weex-template/app.js
+++ weex-template/app.js
@@ -1,3 +1,27 @@
-import foo from './src/foo.vue'
-foo.el = '#root'
-export default new Vue(foo);
+import Home from './src/home.vue'
+import Begin from './src/begin.vue'
+import NotFound from './src/not-found.vue'
+//foo.el = '#root'
+//export default new Vue(foo);
+//const NotFound = { template: '<p>Page not found</p>' }
+//const Home = { template: '<p>home page</p>' }
+//const About = { template: '<p>about page</p>' }
+const routes = {
+  '/weex.html': Home,
+  '/': Home,
+  '/begin': Begin
+}
+
+export default new Vue({
+  el: '#root',
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute] || NotFound
+    }
+  },
+  render (h) { return h(this.ViewComponent) }
+})
+
---
+++ weex-template/src/begin.vue
@@ -0,0 +1,10 @@
+<script>
+  // all this view does is redirect to the play page based on state
+  export default {
+    data: {
+    },
+    methods: {
+      // initialized()?
+    }
+  }
+</script>
